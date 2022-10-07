05b8261f9d88e06c4ed3f6a13284df8b8dd34e50
HBSPS <wiyeong79@naver.com>
2022-10-07 23:23:18 +0900

    :sparkles: [3.1] 모든 게시물을 가져오는 Service 만들기
    [3.1] 모든 게시물을 가져오는 Service 만들기

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,11 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 
 @Controller('boards')
 export class BoardsController {
-    boardsService: BoardsService;
-
-    constructor(boardsService: BoardsService) {
-        this.boardsService = boardsService;
+    constructor(private boardsService: BoardsService) { }
+    
+    @Get('/')
+    getAllBoard() {
+        return this.boardsService.getAllBoards();
     }
 }
