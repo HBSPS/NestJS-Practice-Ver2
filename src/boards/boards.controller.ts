e9684cd0d0a5632c34b9a262442b0a19a0c2cd98
HBSPS <wiyeong79@naver.com>
2022-10-09 01:12:03 +0900

    :sparkles: [3.8] ID로 특정 게시물 지우기
    [3.8] ID로 특정 게시물 지우기

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
 import { Board } from './board.model';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
@@ -20,5 +20,10 @@ export class BoardsController {
     @Get('/:id')
     getBoardById(@Param('id') id: string): Board {
         return this.boardsService.getBoardById(id);
+    }
+
+    @Delete('/:id')
+    deleteBoard(@Param('id') id: string): void {
+        this.boardsService.deleteBoard(id);
     }
 }
