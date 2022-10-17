0e53b7986a5837865242e595efcb806875278340
HBSPS <wiyeong79@naver.com>
2022-10-17 16:43:51 +0900

    :sparkles: [5.3] TypeORM 이용 (+ NestJS 환경변수 설정)
    [5.3] TypeORM 이용 (+ NestJS 환경변수 설정)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardsModule } from './boards/boards.module';
+import { typeORMConfig } from './boards/config/typeorm.config';
 
 @Module({
-  imports: [BoardsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    TypeOrmModule.forRoot(typeORMConfig),
+    BoardsModule],
 })
 export class AppModule {}
