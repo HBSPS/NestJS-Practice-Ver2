a73c14e0e78ed9c54aa465781c11e560f86062dc
HBSPS <wiyeong79@naver.com>
2022-10-24 15:36:04 +0900

    :sparkles: [6.1] ID를 이용해서 특정 게시물 가져오기
    [6.1] ID를 이용해서 특정 게시물 가져오기

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Post, Body, Param, Delete, Patch, ValidationPipe, UsePipes } from '@nestjs/common';
 import { BoardStatus } from './board-status.enum';
+import { Board } from './board.entity';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
@@ -7,6 +8,11 @@ import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe'
 @Controller('boards')
 export class BoardsController {
     constructor(private boardsService: BoardsService) { }
+
+    @Get('/:id')
+    getBoardById(@Param('id') id: number): Promise<Board> {
+        return this.boardsService.getBoardById(id);
+    }
     
     /* @Get('/')
     getAllBoard(): Board[] {
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -2,9 +2,24 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { BoardStatus } from './board-status.enum';
 import { v1 as uuid } from 'uuid';
 import { CreateBoardDto } from './dto/create-board.dto';
+import { BoardRepository } from './board.repository';
+import { Board } from './board.entity';
 
 @Injectable()
 export class BoardsService {
+    constructor(
+        private boardRepository: BoardRepository
+    ) { }
+    
+    async getBoardById(id: number): Promise<Board> {
+        const found = await this.boardRepository.findOne({where: {id: id}});
+
+        if (!found) {
+            throw new NotFoundException(`Can't find Board with id ${id}`);
+        }
+
+        return found;
+    }
     
     /* getAllBoards(): Board[] {
         return this.boards;
